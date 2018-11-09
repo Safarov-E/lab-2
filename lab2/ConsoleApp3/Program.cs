4f73184ad8f9a4fb831f4724212e386b3d8cbd3e
Safarov-E <44256964+Safarov-E@users.noreply.github.com>
2018-11-09 15:13:50 +0500

    Add files via upload

---
+++ lab2/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int Number;
+                int Counter = 0;
+                do
+                {
+                    Number = Int32.Parse(Console.ReadLine());
+                    if (Number % 2 != 0)
+                        Counter++;
+                } while (Number != 0);
+                Console.WriteLine(Counter);
+            }
+        }
+    }
