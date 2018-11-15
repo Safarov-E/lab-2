cea4b7cee7cdfe35841e4fb09bed0e31c22746fb
Safarov-E <44256964+Safarov-E@users.noreply.github.com>
2018-11-15 16:02:13 +0500

    add=

---
+++ lab2-верный/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number;
+            int Counter = 0;
+            do
+            {
+                while (!(int.TryParse(Console.ReadLine(), out Counter)))
+                {
+                    Console.WriteLine("Введите число");
+                }
+                Number = Int32.Parse(Console.ReadLine());
+                if (Number % 2 != 0)
+                    Counter += Number;
+            } while (Number != 0);
+            Console.WriteLine(Counter);
+        }
+    }
+}
